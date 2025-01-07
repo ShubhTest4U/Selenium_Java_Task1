aa8eaf68f33a0e971477cd95894e0225a34cfdb7
ShubhTest4U <shubhamkori63@gmail.com>
2025-01-07 06:27:16 +0530

    updated base url

--- src/main/java/testNG_Program/parameter_method.java
+++ src/main/java/testNG_Program/parameter_method.java
@@ -1,8 +1,16 @@
 package testNG_Program;
 
+import java.time.Duration;
+
 import org.junit.AfterClass;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -14,23 +22,30 @@ public class parameter_method {
 	
 	@Parameters({"base_url"})
 	@Test
-	void open_webpage( String base_url)
+	void open_webpage( String base_url) throws InterruptedException
 	{
 		driver = new ChromeDriver();
 		driver.get(base_url);
 		driver.manage().window().maximize();
+		//Thread.sleep(2000);
 	}
 
 	@Parameters({"Username", "Password"})
 	@Test
 	void double_parameter(String Username, String Password)
 	{
-		System.out.println(Username);
-		System.out.println(Password);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+		WebElement usernameField = wait.until(ExpectedConditions.elementToBeClickable(By.name("username")));
+		usernameField.sendKeys(Username);
+		//driver.findElement(By.name("username")).sendKeys(Username);
+        driver.findElement(By.name("password")).sendKeys(Password);
+        driver.findElement(By.xpath("//button[@type=\"submit\"]")).click();
+		
+		
 	}
 	
 	@Parameters({"Username", "Password", "Login"})
-	@Test
+	@Test(enabled = false)
 	void three_parameter(String Username, String Password, String Login)
 	{
 		System.out.println(Username);
@@ -38,7 +53,7 @@ public class parameter_method {
 		System.out.println(Login);
 	}
 	
-	@AfterClass
+	@AfterTest
 	void close_webpage()
 	{
 		driver.quit();
